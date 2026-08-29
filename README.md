fc733aa712dd0f4004781bb6ad4898c9203d2e7e
LukinhasGsantos <lucasg.santos233@gmail.com>
2026-08-29 09:54:45 -0300

    criado o primeiro servidor com express

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # aula_01_node
 aula 00 de node - backend em js
+
+# comandos básicos
+- git add .
+- git commit -m "mensagem"
+- git push
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Olá mundo!")
+import express from 'express'
+
+const app = express();
+
+app.get('/busca', (req, res) => {
+    console.log(req)
+  res.send('Olá, mundo!')
+})
+
+app.get('/busca/:id', (req, res) => {
+    const numero = req.params.id
+    console.log(numero)
+    const mensagem = `O número requisitado é: ${numero}`
+  res.send(mensagem)
+})
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://127.0.0.1:3000')
+})
